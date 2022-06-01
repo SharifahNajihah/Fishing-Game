c1fa8a518ee6e485b7bea81fd4181bf62105e7da
Jiha <91858830+SharifahNajihah@users.noreply.github.com>
2022-06-01 18:01:13 +0800

    feat : add line renderer

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,12 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System;
 
 namespace FishingGame.Data
 {
-    public class GameController : MonoBehaviour
-    {
-        [SerializeField] Item[] fishItems;
-    }
+
 }
 
